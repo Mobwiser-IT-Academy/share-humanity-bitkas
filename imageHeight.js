63e83299b2b0fa5aed17a894f1beb23686f95d6f
Bitkas <bitokeiro@gmail.com>
2020-08-29 12:21:12 +0100

    added background image

---
+++ imageHeight.js
@@ -0,0 +1,3 @@
+let height = document.getElementById('climateChange').clientHeight;
+//var children = document.getElementById('climateChange').childNodes
+document.getElementById('unica').style.height=((2*height)+40);
