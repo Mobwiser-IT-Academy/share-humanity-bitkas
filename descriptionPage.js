f71fbfcbafea9e8fac39ab301c0f7d7cea53d88f
Bitkas <bitokeiro@gmail.com>
2020-09-03 22:09:13 +0100

    added eslint, prettier, package.json

--- descriptionPage.js
+++ descriptionPage.js
@@ -2,33 +2,33 @@
 //let page1 = path.split("=").pop();
 let search = window.location.search;
 let result = search.split("=").pop();
-switch(result) {
-	case "climateChange":
-		climate1();
-		break;
-		
-	case "extremePoverty":
-		poverty();
-		break;
+switch (result) {
+  case "climateChange":
+    climate1();
+    break;
+
+  case "extremePoverty":
+    poverty();
+    break;
 }
 function climate1() {
-	let h4 = document.createElement("h4");
-	let a = document.createTextNode(climate.nome);
-	h4.appendChild(a);
-	let p = document.createElement("p");
-	let b = document.createTextNode(climate.description);
-	p.appendChild(b);
-	document.getElementById("detail").appendChild(h4);
-	document.getElementById("detail").appendChild(p);
+  let h4 = document.createElement("h4");
+  let a = document.createTextNode(climate.nome);
+  h4.appendChild(a);
+  let p = document.createElement("p");
+  let b = document.createTextNode(climate.description);
+  p.appendChild(b);
+  document.getElementById("detail").appendChild(h4);
+  document.getElementById("detail").appendChild(p);
 }
 
 function poverty() {
-	let h4 = document.createElement("h4");
-	let a = document.createTextNode(extremePoverty.nome);
-	h4.appendChild(a);
-	let p = document.createElement("p");
-	let b = document.createTextNode(extremePoverty.description);
-	p.appendChild(b);
-	document.getElementById("detail").appendChild(h4);
-	document.getElementById("detail").appendChild(p);
+  let h4 = document.createElement("h4");
+  let a = document.createTextNode(extremePoverty.nome);
+  h4.appendChild(a);
+  let p = document.createElement("p");
+  let b = document.createTextNode(extremePoverty.description);
+  p.appendChild(b);
+  document.getElementById("detail").appendChild(h4);
+  document.getElementById("detail").appendChild(p);
 }
--- imageHeight.js
+++ imageHeight.js
@@ -1,7 +1,7 @@
-let children = document.getElementById('unica').lastElementChild;
-let father = document.getElementById('unica');
+let children = document.getElementById("unica").lastElementChild;
+let father = document.getElementById("unica");
 let fatherPosition = father.getBoundingClientRect();
 let lastChildrenPosition = children.getBoundingClientRect();
-let firstPosition = lastChildrenPosition.top-fatherPosition.top;
+let firstPosition = lastChildrenPosition.top - fatherPosition.top;
 let secondPosition = children.height;
-document.getElementById('unica').style.height=(firstPosition+secondPosition);
+document.getElementById("unica").style.height = firstPosition + secondPosition;
