3d61f708553d2e6c86da70bbe7cffd72d56bf2e2
Bitkas <bitokeiro@gmail.com>
2020-09-28 15:32:13 +0100

    mudei entry point de webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 
-    entry: './mainScreen/generator.ts',
+    entry: './mainScreen/filter.ts',
     devtool: 'inline-smainScreenource-map',
     devServer: {
         publicPath : '/dist/',
