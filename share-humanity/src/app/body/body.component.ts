aa0b8ca1cbdb1e55ffcbdab4f23a3f4549eb741d
Bitkas <bitokeiro@gmail.com>
2020-12-02 15:05:19 +0000

    added animations

--- share-humanity/src/app/body/body.component.ts
+++ share-humanity/src/app/body/body.component.ts
@@ -2,11 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database'
 import { Observable } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('1.5s 0.1s ease-in')
+      ]),
+      transition('* => void', [
+        animate('1.5s 0.1s ease-in', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class BodyComponent implements OnInit {
 
