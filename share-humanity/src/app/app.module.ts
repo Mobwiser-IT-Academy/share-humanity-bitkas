209d677b5bce759b8411ed1829864a3f28d1cc9f
Bitkas <bitokeiro@gmail.com>
2020-12-02 09:47:50 +0000

    criando components

--- share-humanity/src/app/app.module.ts
+++ share-humanity/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
