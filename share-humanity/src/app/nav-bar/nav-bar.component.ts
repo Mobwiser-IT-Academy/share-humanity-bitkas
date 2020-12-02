b137cfa1712ba6709261dd65066322e60808a359
Bitkas <bitokeiro@gmail.com>
2020-12-02 11:54:38 +0000

    navBar working

--- share-humanity/src/app/nav-bar/nav-bar.component.ts
+++ share-humanity/src/app/nav-bar/nav-bar.component.ts
@@ -12,4 +12,18 @@ export class NavBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  changeNavBar() {
+    let nav = document.getElementById('topnav')!;
+    if(nav.className === 'topnav') {
+      nav.className += ' responsive';
+      nav.style.height = "100px";
+      nav.style.display = "block";
+    } else {
+      nav.className = 'topnav';
+      nav.style.height = "50px";
+      nav.style.display = "flex";
+    }
+    
+  }
+
 }
