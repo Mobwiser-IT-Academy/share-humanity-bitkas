a8a15cce5f596328ef5db1c014c5aec72ef40ab4
Bitkas <bitokeiro@gmail.com>
2020-12-04 17:05:20 +0000

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import{ DescriptionComponent } from './description/description.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component: BodyComponent},
+  { path: 'description/:name' , component: DescriptionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireService } from '../angular-fire.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-description',
@@ -8,12 +9,19 @@ import { AngularFireService } from '../angular-fire.service';
   styleUrls: ['./description.component.css']
 })
 export class DescriptionComponent implements OnInit {
+  route: any;
+  name : any;
   item : Observable<any[]>;
-  constructor(item : AngularFireService) {
+  constructor(item : AngularFireService, routes: ActivatedRoute) {
     this.item = item.items;
+    this.route = routes;
    }
   
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: any) => {
+      this.name = params['name'];
+      console.log(name);
+    });
   }
 
 }
