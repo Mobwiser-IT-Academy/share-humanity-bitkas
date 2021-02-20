b9ed629913223652a79a1520c81c051404cb81bc
Bitkas <bitokeiro@gmail.com>
2021-02-20 18:28:00 +0000

    Auto-generated commit

--- mainScreen/generator.d.ts
+++
@@ -1,1 +0,0 @@
-declare function myFunction(parentId: string): void;
--- mainScreen/globalIssues.d.ts
+++
@@ -1,9 +0,0 @@
-interface GlobalIssue {
-    nome: string;
-    afetados: string;
-    description: string;
-    rank: number;
-}
-declare let globalIssues: Array<GlobalIssue>;
-export { GlobalIssue };
-export { globalIssues };
