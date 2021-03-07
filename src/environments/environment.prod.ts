0c566a1a8c75b87853bf466ef7b80ae6c0172253
Bitkas <bitokeiro@gmail.com>
2021-03-07 19:08:33 +0000

    ng deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDQUtzp6LJyLpbDu5xWJUs5SYW51LJUq5c",
+    authDomain: "angular-3bb22.firebaseapp.com",
+    databaseURL: "https://angular-3bb22.firebaseio.com",
+    projectId: "angular-3bb22",
+    storageBucket: "angular-3bb22.appspot.com",
+    messagingSenderId: "1041903246338",
+    appId: "1:1041903246338:web:7d824b4266d667a83edb1b",
+    measurementId: "G-BE1J4W4ZYJ"
+  }
 };
